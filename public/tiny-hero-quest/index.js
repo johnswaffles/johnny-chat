5ed719391a1eab6a7868a48ac4c6aeadfe5268ed
John Shopinski <johnswaffles@gmail.com>
2026-04-30 10:52:20 -0500

    Deploy Tiny Hero wizard update

--- public/tiny-hero-quest/index.js
+++ public/tiny-hero-quest/index.js
@@ -558,17 +558,17 @@ const InternalConfig = function (initConfig) { // eslint-disable-line no-unused-
 				if (!path.startsWith('godot.')) {
 					return path;
 				} else if (path.endsWith('.audio.worklet.js')) {
-					return "https://johnny-chat.onrender.com/tiny-hero-quest/index.audio.worklet.js?v=molm2xs4";
+					return "https://johnny-chat.onrender.com/tiny-hero-quest/index.audio.worklet.js?v=molndevj";
 				} else if (path.endsWith('.audio.position.worklet.js')) {
-					return "https://johnny-chat.onrender.com/tiny-hero-quest/index.audio.position.worklet.js?v=molm2xs4";
+					return "https://johnny-chat.onrender.com/tiny-hero-quest/index.audio.position.worklet.js?v=molndevj";
 				} else if (path.endsWith('.js')) {
 					return `${loadPath}.js`;
 				} else if (path in gdext) {
 					return path;
 				} else if (path.endsWith('.side.wasm')) {
-					return "https://johnny-chat.onrender.com/tiny-hero-quest/index.side.wasm?v=molm2xs4";
+					return "https://johnny-chat.onrender.com/tiny-hero-quest/index.side.wasm?v=molndevj";
 				} else if (path.endsWith('.wasm')) {
-					return "https://johnny-chat.onrender.com/tiny-hero-quest/index.wasm?v=molm2xs4";
+					return "https://johnny-chat.onrender.com/tiny-hero-quest/index.wasm?v=molndevj";
 				}
 				return path;
 			},
@@ -671,7 +671,7 @@ const Engine = (function () {
 	Engine.load = function (basePath, size) {
 		if (loadPromise == null) {
 			loadPath = basePath;
-			loadPromise = preloader.loadPromise("https://johnny-chat.onrender.com/tiny-hero-quest/index.wasm?v=molm2xs4", size, true);
+			loadPromise = preloader.loadPromise("https://johnny-chat.onrender.com/tiny-hero-quest/index.wasm?v=molndevj", size, true);
 			requestAnimationFrame(preloader.animateProgress);
 		}
 		return loadPromise;
@@ -826,7 +826,7 @@ const Engine = (function () {
 				// Add main-pack argument.
 				const exe = this.config.executable;
 				const pack = this.config.mainPack || `${exe}.pck`;
-				const packSource = this.config.mainPack || "https://johnny-chat.onrender.com/tiny-hero-quest/index.pck?v=molm2xs4";
+				const packSource = this.config.mainPack || "https://johnny-chat.onrender.com/tiny-hero-quest/index.pck?v=molndevj";
 				this.config.args = ['--main-pack', pack].concat(this.config.args);
 				// Start and init with execName as loadPath if not inited.
 				const me = this;
